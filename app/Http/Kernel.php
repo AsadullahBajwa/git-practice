08491830aeb5eb34ca24d8c303d21426a906dd08
AsadullahBajwa <asadullahbajwa76@gmail.com>
2023-08-21 13:04:52 +0500

    Practice of Middleware all three types commit 2

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -45,7 +45,7 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
         'protectedPage'=>[
-            \App\Http\Middleware\checkAge2::class,
+            \App\Http\Middleware\checkAgeGrp::class,
         ]
     ];
 
@@ -57,7 +57,7 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
-        'protectPage' => \App\Http\Middleware\checkage3::class,
+        'protectPage' => \App\Http\Middleware\checkAgeRoute::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
--- app/Http/Middleware/checkAgeGrp.php
+++ app/Http/Middleware/checkAgeGrp.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 
-class checkage3
+class checkAgeGrp
 {
     /**
      * Handle an incoming request.
